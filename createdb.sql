3be373172d0f77678c49ba6256776ebe1f0e4b2d
Dinesh130914 <dineshthiyagu99@gmail.com>
2022-01-21 15:13:53 +0530

    Create database of emp_payroll

---
+++ createdb.sql
@@ -0,0 +1,22 @@
+#UC-1
+
+mysql> create database AddressBook_Service;
+Query OK, 1 row affected (0.10 sec)
+
+mysql> show databases;
++---------------------+
+| Database            |
++---------------------+
+| addressbook_service |
+| information_schema  |
+| mysql               |
+| payroll_service     |
+| performance_schema  |
+| sakila              |
+| sys                 |
+| world               |
++---------------------+
+8 rows in set (0.05 sec)
+
+mysql> use addressbook_service;
+Database changed
